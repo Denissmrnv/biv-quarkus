672d66e7b399ebbf44019fb649d9f3a8eb735d17
Denissmrnv <87768234+Denissmrnv@users.noreply.github.com>
2023-07-10 16:49:23 +0300

    feat: Добавил метрики на все эндпоинты продуктов

--- src/main/java/edu/my/controller/ProductController.java
+++ src/main/java/edu/my/controller/ProductController.java
@@ -6,6 +6,9 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import java.util.List;
 
@@ -19,18 +22,24 @@ public class ProductController {
 
     @GET
     @Path("/products")
+    @Counted(name = "performedChecksGetAllProducts", description = "How many requests of all products were made.")
+    @Timed(name = "checksTimerGetAllProducts", description = "A measure of how long it takes to complete a query of all products.", unit = MetricUnits.MILLISECONDS)
     public List<ProductDTO> getAllProducts()  {
         return productService.getAllProducts();
     }
 
     @GET
     @Path("/products/{id}")
+    @Counted(name = "performedChecksGetOneProduct", description = "How many requests of one product were made.")
+    @Timed(name = "checksTimerGetOneProduct", description = "A measure of how long it takes to complete a query of one product.", unit = MetricUnits.MILLISECONDS)
     public ProductDTO getProductById(@PathParam("id") Long id)  {
         return productService.getProduct(id);
     }
 
     @POST
     @Path("/products")
+    @Counted(name = "performedChecksAddProduct", description = "How many product additions have been made.")
+    @Timed(name = "checksTimerAddProduct", description = "A measure of how long it takes to complete a product addition.", unit = MetricUnits.MILLISECONDS)
     public String addProduct(ProductDTO productDTO) {
         productService.saveProduct(productDTO);
         return "Product is added";
@@ -38,6 +47,8 @@ public class ProductController {
 
     @DELETE
     @Path("/products/{id}")
+    @Counted(name = "performedChecksDeleteProduct", description = "How many product deleted have been made.")
+    @Timed(name = "checksTimerDeleteProduct", description = "A measure of how long it takes to complete a product delete.", unit = MetricUnits.MILLISECONDS)
     public String deleteProduct(@PathParam("id") long id) {
         productService.deleteProduct(id);
         return "Product is deleted";
@@ -45,6 +56,8 @@ public class ProductController {
 
     @PUT
     @Path("/products/{id}")
+    @Counted(name = "performedChecksUpdateProduct", description = "How many product updated have been made.")
+    @Timed(name = "checksTimerUpdateProduct", description = "A measure of how long it takes to complete a product update.", unit = MetricUnits.MILLISECONDS)
     public String updateProduct(@PathParam("id") long id, ProductDTO productDTO) {
         productService.updateProduct(id, productDTO);
         return "Product is updated";
