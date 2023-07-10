8ef7e3d9010fc31243dcde792021d483474ccdb3
Denissmrnv <87768234+Denissmrnv@users.noreply.github.com>
2023-07-10 23:04:00 +0300

    feat: Описал openapi для продукта

--- src/main/java/edu/my/controller/ProductController.java
+++ src/main/java/edu/my/controller/ProductController.java
@@ -6,9 +6,17 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import java.util.List;
 
@@ -16,6 +24,7 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("/")
+@Tag(name = "Product Resource", description = "Product REST APIs")
 public class ProductController {
     @Inject
     ProductService productService;
@@ -24,41 +33,107 @@ public class ProductController {
     @Path("/products")
     @Counted(name = "performedChecksGetAllProducts", description = "How many requests of all products were made.")
     @Timed(name = "checksTimerGetAllProducts", description = "A measure of how long it takes to complete a query of all products.", unit = MetricUnits.MILLISECONDS)
-    public List<ProductDTO> getAllProducts()  {
-        return productService.getAllProducts();
+    @Operation(
+            operationId = "getAllProducts",
+            summary = "Get all Products",
+            description = "Get all products inside the db"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Operation completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public Response getAllProducts()  {
+        return Response.ok(productService.getAllProducts()).build();
     }
 
     @GET
     @Path("/products/{id}")
     @Counted(name = "performedChecksGetOneProduct", description = "How many requests of one product were made.")
     @Timed(name = "checksTimerGetOneProduct", description = "A measure of how long it takes to complete a query of one product.", unit = MetricUnits.MILLISECONDS)
-    public ProductDTO getProductById(@PathParam("id") Long id)  {
-        return productService.getProduct(id);
+    @Operation(
+            operationId = "getProductById",
+            summary = "Get Product by id",
+            description = "Get one product inside the db"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Operation completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public Response getProductById(@PathParam("id") Long id)  {
+        return Response.ok(productService.getProduct(id)).build();
     }
 
     @POST
     @Path("/products")
     @Counted(name = "performedChecksAddProduct", description = "How many product additions have been made.")
     @Timed(name = "checksTimerAddProduct", description = "A measure of how long it takes to complete a product addition.", unit = MetricUnits.MILLISECONDS)
-    public String addProduct(ProductDTO productDTO) {
+    @Operation(
+            operationId = "addProduct",
+            summary = "Add a new Product",
+            description = "Add a new product to the db"
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Product is added",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public Response addProduct(
+            @RequestBody(
+                    description = "Product to create",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = ProductDTO.class))
+            )
+            ProductDTO productDTO) {
         productService.saveProduct(productDTO);
-        return "Product is added";
+        return Response.status(Response.Status.CREATED).entity(productService.getAllProducts()).build();
     }
 
     @DELETE
     @Path("/products/{id}")
     @Counted(name = "performedChecksDeleteProduct", description = "How many product deleted have been made.")
     @Timed(name = "checksTimerDeleteProduct", description = "A measure of how long it takes to complete a product delete.", unit = MetricUnits.MILLISECONDS)
-    public String deleteProduct(@PathParam("id") long id) {
+    @Operation(
+            operationId = "deleteProduct",
+            summary = "Delete an existing Product",
+            description = "Delete a product inside the db"
+    )
+    @APIResponse(
+            responseCode = "204",
+            description = "Product is deleted",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public Response deleteProduct(@PathParam("id") long id) {
         productService.deleteProduct(id);
-        return "Product is deleted";
+        return Response.noContent().build();
     }
 
     @PUT
     @Path("/products/{id}")
     @Counted(name = "performedChecksUpdateProduct", description = "How many product updated have been made.")
     @Timed(name = "checksTimerUpdateProduct", description = "A measure of how long it takes to complete a product update.", unit = MetricUnits.MILLISECONDS)
-    public String updateProduct(@PathParam("id") long id, ProductDTO productDTO) {
+    @Operation(
+            operationId = "updateProduct",
+            summary = "Update an existing Product",
+            description = "Update a product inside the db"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Product is updated",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public String updateProduct(
+            @Parameter(
+                    description = "Product id",
+                    required = true
+            )
+            @PathParam("id") long id,
+            @Parameter(
+                    description = "Updated Product",
+                    required = true
+            )
+            ProductDTO productDTO) {
         productService.updateProduct(id, productDTO);
         return "Product is updated";
     }
--- src/main/java/edu/my/dto/ProductDTO.java
+++ src/main/java/edu/my/dto/ProductDTO.java
@@ -3,6 +3,7 @@ package edu.my.dto;
 import edu.my.entity.Category;
 import edu.my.entity.Characteristic;
 import lombok.*;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.Set;
 
@@ -10,11 +11,18 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "ProductDTO", description = "Product representation")
 public class ProductDTO {
+    @Schema(description = "Product id")
     private Long id;
+    @Schema(description = "Product name")
     private String name;
+    @Schema(description = "Product price")
     private Float price;
+    @Schema(description = "Product description")
     private String description;
+    @Schema(description = "Product object Category")
     private Category category;
+    @Schema(description = "Product set of characteristic")
     private Set<Characteristic> characteristicSet;
 }
