a6969f0bbd6602806400f39eb197e108b73b04b8
Denissmrnv <87768234+Denissmrnv@users.noreply.github.com>
2023-07-10 16:31:41 +0300

    feat: Добавил метрики на все эндпоинты характеристики

--- src/main/java/edu/my/controller/CharacteristicController.java
+++ src/main/java/edu/my/controller/CharacteristicController.java
@@ -7,6 +7,9 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import java.util.List;
 
@@ -20,18 +23,24 @@ public class CharacteristicController {
 
     @GET
     @Path("/characteristics")
+    @Counted(name = "performedChecksGetAllCharacteristics", description = "How many requests of all characteristics were made.")
+    @Timed(name = "checksTimerGetAllCharacteristics", description = "A measure of how long it takes to complete a query of all characteristics.", unit = MetricUnits.MILLISECONDS)
     public List<CharacteristicDTO> getAllCharacteristics()  {
         return characteristicService.getAllCharacteristics();
     }
 
     @GET
     @Path("/characteristics/{id}")
+    @Counted(name = "performedChecksGetOneCharacteristic", description = "How many requests of one characteristic were made.")
+    @Timed(name = "checksTimerGetOneCharacteristic", description = "A measure of how long it takes to complete a query of one characteristic.", unit = MetricUnits.MILLISECONDS)
     public CharacteristicDTO getCharacteristicById(@PathParam("id") Long id)  {
         return characteristicService.getCharacteristic(id);
     }
 
     @POST
     @Path("/characteristics")
+    @Counted(name = "performedChecksAddCharacteristic", description = "How many characteristic additions have been made.")
+    @Timed(name = "checksTimerAddCharacteristic", description = "A measure of how long it takes to complete a characteristic addition.", unit = MetricUnits.MILLISECONDS)
     public String addCharacteristic(CharacteristicDTO characteristicDTO) {
         characteristicService.saveCharacteristic(characteristicDTO);
         return "Characteristic is added";
@@ -39,6 +48,8 @@ public class CharacteristicController {
 
     @DELETE
     @Path("/characteristics/{id}")
+    @Counted(name = "performedChecksDeleteCharacteristic", description = "How many characteristic deleted have been made.")
+    @Timed(name = "checksTimerDeleteCharacteristic", description = "A measure of how long it takes to complete a characteristic delete.", unit = MetricUnits.MILLISECONDS)
     public String deleteCharacteristic(@PathParam("id") long id) {
         characteristicService.deleteCharacteristic(id);
         return "Characteristic is deleted";
@@ -46,6 +57,8 @@ public class CharacteristicController {
 
     @PUT
     @Path("/characteristics/{id}")
+    @Counted(name = "performedChecksUpdateCharacteristic", description = "How many characteristic updated have been made.")
+    @Timed(name = "checksTimerUpdateCharacteristic", description = "A measure of how long it takes to complete a characteristic update.", unit = MetricUnits.MILLISECONDS)
     public String updateCharacteristic(@PathParam("id") long id, CharacteristicDTO characteristicDTO) {
         characteristicService.updateCharacteristic(id, characteristicDTO);
         return "Characteristic is updated";
